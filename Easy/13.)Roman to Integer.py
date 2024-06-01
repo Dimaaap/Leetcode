4ff651f543f0888b4de44bf032a3d158049fd04c
Dimaaap <procdima49@gmail.com>
2024-06-01 23:42:14 +0300

    Some changes

--- Easy/13.)Roman to Integer.py
+++ Easy/13.)Roman to Integer.py
@@ -1,7 +1,4 @@
 def roman_to_int(s: str):
-    """
-    Given a roman numeral, convert it to an integer.
-    """
     convert_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
                           'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
                           'CD': 400, 'CM': 900}
