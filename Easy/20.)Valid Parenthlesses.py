52519b64c561831d06a2684b6cdb9f78ee87d202
Dimaaap <procdima49@gmail.com>
2024-06-08 23:43:58 +0300

    Small changes

--- Easy/20.)Valid Parenthlesses.py
+++ Easy/20.)Valid Parenthlesses.py
@@ -1,8 +1,4 @@
 def is_valid(s: str):
-    """
-    Write a function to find the longest common prefix string amongst an array of strings.
-    If there is no common prefix, return an empty string "".
-    """
     parentheses = {"(": ")", "{": "}", "[": "]"}
     queue = []
     for i in s:
