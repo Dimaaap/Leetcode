40976a94ced605558470e6e756bfff1bb6c5d5ab
Dimaaap <procdima49@gmail.com>
2026-09-12 09:28:21 +0300

    Added description to task

--- Easy/1071.)Greatest Common Divisor of Strings.py
+++ Easy/1071.)Greatest Common Divisor of Strings.py
@@ -1,4 +1,10 @@
 def gcd_of_strings(str1: str, str2: str) -> str:
+    """
+    For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t
+    (i.e., t is concatenated with itself one or more times).
+    Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+    """
+    
     if str1 + str2 != str2 + str1:
         return ""
 
